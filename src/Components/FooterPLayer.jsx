83572963799671357a3498d49c26d90ab3ee4df0
Kaua <kauareysan@gmail.com>
2023-12-28 14:15:32 -0300

    adicionando area de musica no player

--- src/Components/FooterPLayer.jsx
+++ src/Components/FooterPLayer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FooterPlayer.css"
 
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -14,12 +14,31 @@ import DevicesIcon from '@mui/icons-material/Devices';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import LoopIcon from '@mui/icons-material/Loop';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 export const FooterPLayer = () => {
+
+    const [repIcon, setRepIcon] = useState(false)
+
+    const song = {
+        name: "Walk",
+        artist: "Foo Fighters",
+        img: "/public/wl.jpeg"
+    }
+
     return(
         <div className="container-player">
             <div className="first-part">
-                <FavoriteIcon/>
+                <div className="song-area">
+                    <img src={song.img} alt="" />
+                    <div className="s-g-text-area">
+                        <p id="musica-name">{song.name}</p>
+                        <p id="artist-name">{song.artist}</p>
+                    </div>
+                    <div className="s-g-icon-area" onClick={e => setRepIcon(!repIcon)}>
+                        {repIcon ? <FavoriteIcon style={{width: "20px", height: "20px", color: "rgb(33, 153, 33)"}}/> : <FavoriteBorderOutlinedIcon style={{width: "20px", height: "20px"}}/>}
+                    </div>
+                </div>
             </div>
             <div className="middle">
                 <ShuffleIcon/>
